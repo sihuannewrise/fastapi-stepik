707fc37c874775561a61ebaec524b62949c75f3e
Sihuan <sihuannewrise@gmail.com>
2026-08-23 23:16:51 +0300

    processPoolExecutor cb

--- async/part3/3_15_2_cpu_bound.py
+++ async/part3/3_15_2_cpu_bound.py
@@ -3,6 +3,9 @@ from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 entities = []
 
+async def cb(task):
+
+
 
 async def main():
     tasks = []
@@ -10,11 +13,11 @@ async def main():
     with ThreadPoolExecutor() as th_pool, ProcessPoolExecutor() as pr_pool:
         for entity in entities:
             if asyncio.iscoroutine(entity):
-                tasks.append(asyncio.create_task(entity))
+                tasks.append(asyncio.create_task(entity).add_done_callback(cb))
             elif hasattr(entity, "cpu"):
-                tasks.append(loop.run_in_executor(pr_pool, entity))
+                tasks.append(loop.run_in_executor(pr_pool, entity).add_done_callback(cb))
             else:
-                tasks.append(loop.run_in_executor(th_pool, entity))
+                tasks.append(loop.run_in_executor(th_pool, entity).add_done_callback(cb))
         await asyncio.gather(*tasks, return_exceptions=True)
 
 
