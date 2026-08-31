f21e0ea6e6d0ce786da2ac41df89801132481358
sihuannewrise <sihuannewrise@gmail.com>
2026-08-31 22:35:58 +0300

    pririty compact

--- async/part3/3_20_priorityQueue.py
+++ async/part3/3_20_priorityQueue.py
@@ -1,9 +1,5 @@
 import asyncio
 
-async def json_gen(): pass
-async def final(): pass
-async def registrator(): pass
-
 
 async def producer(queue: asyncio.Queue):
     async for elem in json_gen():
@@ -18,15 +14,7 @@ async def consumer(queue: asyncio.Queue):
 
 
 async def producer_consumer(queue: asyncio.Queue):
-    producer_task = asyncio.create_task(producer(queue))
-    consumer_tasks = [asyncio.create_task(consumer(queue)) for _ in range(2)]
-
-    await producer_task
+    [asyncio.create_task(consumer(queue)) for _ in range(2)]
+    await asyncio.create_task(producer(queue))
     await queue.join()
-
-    for task in consumer_tasks:
-        task.cancel()
-
-    await asyncio.gather(*consumer_tasks, return_exceptions=True)
-
     final()
